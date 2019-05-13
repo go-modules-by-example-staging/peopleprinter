b31f82872a08cf4288e68a7836553f7fd73aefcd
myitcv <myitcv@example.com>
2019-05-13 14:56:49 +0000

    Use goinfo v2 in peopleprinter

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/go-modules-by-example-staging/peopleprinter
 
 go 1.12
 
-require github.com/go-modules-by-example-staging/goinfo v1.0.0
+require (
+	github.com/go-modules-by-example-staging/goinfo v1.0.0
+	github.com/go-modules-by-example-staging/goinfo/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/go-modules-by-example-staging/goinfo/designers"
+	v2designers "github.com/go-modules-by-example-staging/goinfo/v2/designers"
 	"strings"
 )
 
 func main() {
 	fmt.Printf("The designers of Go: %v\n", strings.Join(designers.Names(), ", "))
+	fmt.Printf("The designers of Go: %v\n", strings.Join(v2designers.FullNames(), ", "))
 }
